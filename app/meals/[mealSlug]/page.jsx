2afc4ddf505a7a975a766b1dbc403afcc1511216
Russ485 <kazyonnylawyer@gmail.com>
2024-03-07 13:10:32 +0100

    Remove unused `generateMetadata` function.

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -4,18 +4,18 @@ import Image from "next/image";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
-export async function generateMetadata({ params }) {
-  const meal = getMeal(params.mealSlug);
+// export async function generateMetadata({ params }) {
+//   const meal = getMeal(params.mealSlug);
 
-  if (!meal) {
-    notFound();
-  }
+//   if (!meal) {
+//     notFound();
+//   }
 
-  return {
-    title: meal.title,
-    description: meal.summary,
-  };
-}
+//   return {
+//     title: meal.title,
+//     description: meal.summary,
+//   };
+// }
 
 export default function MealDetailPage({ params }) {
   const meal = getMeal(params.mealSlug);
